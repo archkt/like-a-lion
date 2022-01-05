f4e51cb52abc7627e6c4a87fb0ef7d26d879f049
archkt <archkt@berkeley.edu>
2022-01-04 17:35:24 -0800

    [feature] nav bar implemented

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import *
+from pymongo import MongoClient
+
+client = MongoClient("localhost", 27017)
+db = client.likelion
 
 app = Flask(__name__)
 app.secret_key = "test"
@@ -11,9 +15,15 @@ def index():
     login_status = "login" in session and session["login"] == 'true'
     print(login_status)
     if login_status:
-        return render_template('index.html')
+        all_db = list(db.likelion.find({},{'_id':0}))
+        return redirect(url_for('login'))
     else:
         return redirect(url_for('login'))
+
+@app.route('/calendar')
+def cal():
+    all_db = list(db.likelion.find({},{'_id':0}))
+    return render_template('index.html', all_data = all_db)
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
@@ -24,9 +34,10 @@ def login():
     else:
         login_status = "login" in session and session["login"] == 'true'
         if login_status:
-            return redirect(url_for('index'))
+            return redirect(url_for('cal'))
         else:
             return render_template('login.html')
+    
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
